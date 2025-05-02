259b771a07bf3399710101caca214345fcef9ec6
Pratham Kave <kavepratham@gmail.com>
2025-05-02 11:18:50 +0530

    Conditional Has Added

---
+++ src/Core/Conditional.java
@@ -0,0 +1,11 @@
+package Core;
+
+public class Conditional {
+    public static void main(String[] args) {
+        int a = 18;
+
+        if (a % 2 == 0) {
+            System.out.println("Number is Even");
+        }
+    }
+}
--- src/Core/Day1.java
+++ src/Core/Day1.java
@@ -1,4 +1,7 @@
 package Core;
 
 public class Day1 {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
 }
