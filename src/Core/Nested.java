b882927f0fb68f3c7b5305c094e31d6123131c1f
Pratham Kave <kavepratham@gmail.com>
2025-05-02 11:39:10 +0530

    Nested Done

---
+++ src/Core/Nested.java
@@ -0,0 +1,13 @@
+package Core;
+
+public class Nested {
+    public static void main(String[] args) {
+        int a = 20;
+
+        if (a == 20) {
+            if (a < 30) {
+                System.out.println("From Nested");
+            }
+        }
+    }
+}
