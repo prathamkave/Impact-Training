3c005ad559f199720ac3f58c974bad0a4f54dae9
Pratham Kave <kavepratham@gmail.com>
2025-05-02 11:28:56 +0530

    if-else done

--- src/Core/Conditional.java
+++ src/Core/Conditional.java
@@ -2,10 +2,13 @@ package Core;
 
 public class Conditional {
     public static void main(String[] args) {
-        int a = 18;
+        int a = 19;
 
         if (a % 2 == 0) {
-            System.out.println("Number is Even");
+            System.out.println("Number is Even.");
+        }
+        else {
+            System.out.println("Number is Odd.");
         }
     }
 }
